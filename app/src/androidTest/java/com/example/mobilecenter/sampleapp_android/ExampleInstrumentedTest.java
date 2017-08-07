6b975b8266698b9e952a4ed62be322ece07192ed
sabrinaj206 <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-08-07 09:58:48 -0700

    added instrumentation tag

--- app/src/androidTest/java/com/example/mobilecenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mobilecenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
+
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +21,19 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.tina.sampleapp_android", appContext.getPackageName());
+        assertEquals("com.example.mobilecenter.sampleapp_android", appContext.getPackageName());
     }
 }
